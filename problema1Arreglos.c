cddb55f6bb7c1b44c899fc472b21b458720cf56b
DART1511 <ramirez.t.david510@gmail.com>
2025-11-21 23:42:42 -0600

    Create problema1Arreglos.c

---
+++ problema1Arreglos.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int i;
+    int suma = 0;
+
+    printf("Ingrese el tamaño del arreglo: ");
+    scanf("%d", &n);
+
+    int arreglo[n];
+
+    for (i = 0; i < n; i++)
+    {
+        printf("Elemento %d: ", i + 1);
+        scanf("%d", &arreglo[i]);
+    }
+
+    for (i = 0; i < n; i++)
+        suma += arreglo[i];
+
+    printf("La suma de los elementos es: %d\n", suma);
+
+    return 0;
+}
