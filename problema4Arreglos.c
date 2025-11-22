0ada2485793dda81f9528781152c55cae1ca889e
DART1511 <ramirez.t.david510@gmail.com>
2025-11-21 23:48:30 -0600

    Create problema4Arreglos.c

---
+++ problema4Arreglos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int pares[100];
+    int i;
+
+    for (i = 0; i < 100; i++)
+        pares[i] = (i + 1) * 2;
+
+    for (i = 0; i < 100; i++)
+        printf("%d\n", pares[i]);
+
+    return 0;
+}
