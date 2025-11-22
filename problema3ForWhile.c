3685531c4e75047017219eec451102ad6beb44a1
DART1511 <ramirez.t.david510@gmail.com>
2025-11-21 23:38:08 -0600

    Create problema3ForWhile.c

---
+++ problema3ForWhile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    int suma = 0;
+
+    for (i = 10; i <= 50; i += 2)
+        suma += i;
+
+    printf("La suma de los numeros pares entre 10 y 50 es: %d\n", suma);
+
+    return 0;
+}
